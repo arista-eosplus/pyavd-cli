1184741a4c57747ed5d231c342b4102ed998d64c
David Lobato <dlobato@arista.com>
2024-03-07 17:23:16 +0000

    Add LICENSE (#5)
    Add pre-commit hooks to add license-short.txt
    Cleanups

--- pyavd_cli/__init__.py
+++ pyavd_cli/__init__.py
@@ -1,1 +1,3 @@
-"""pyavd_cli.__init__."""
+# Copyright (c) 2024 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
--- pyavd_cli/build.py
+++ pyavd_cli/build.py
@@ -1,4 +1,6 @@
-# pylint: disable=missing-module-docstring,missing-function-docstring
+# Copyright (c) 2024 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
 
 import argparse
 import logging
